533b32717070cac07bd25d2fd686497c8089a6c7
Pixelpato <pixelpato@gmail.com>
2021-02-01 18:58:19 +0100

    project setup with google vr sdk

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public bool turnOnly;
+
+    private Vector3 forward;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void LateUpdate()
+    {
+        if (turnOnly)
+        {
+            forward = new Vector3(Camera.main.transform.forward.x, transform.forward.y, Camera.main.transform.forward.z);
+            Vector3 camPos = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
+
+            if (forward != Vector3.zero)
+            {
+                // transform.forward = forward;
+                transform.LookAt(camPos, Vector3.up);
+            }
+        }
+        else
+        {
+            //transform.LookAt(Camera.main.transform.position, Vector3.up);
+            transform.forward = Camera.main.transform.forward;
+        }
+    }
+}
